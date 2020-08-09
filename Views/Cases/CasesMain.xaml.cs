157dca8a81c94f26b8bd54a09bb88074cbeb8a34
Nawzat <nawzat.sibai@gmail.com>
2020-08-09 12:42:33 +0300

    Project Structure and UIs

---
+++ Views/Cases/CasesMain.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LegalAffairs.Views
+{
+    /// <summary>
+    /// Interaction logic for CasesMain.xaml
+    /// </summary>
+    public partial class CasesMain : Window
+    {
+        public CasesMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
